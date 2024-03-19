7de7f32e49d6c638ae85b20cc7bfdf4b5ae7948a
XWBSZ <xwbszoom@gmail.com>
2024-03-19 12:58:37 +0800

    基础函数写好了

---
+++ BLCT/启动.js
@@ -0,0 +1,6 @@
+const { clickImageWithTimeout, swipe360, zoom, tessocr, clickOnLine } = require('./baseFunctions');
+
+function RUN() {
+    app.launch('com.supercell.clashofclans');
+}
+RUN(); // 执行运行函数
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BLCTONE
 尝试一个文件写完部落冲突自动化
+/$MuMu12Shared/AUTOX/BLCTONE/BLCT
